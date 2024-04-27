19c838bab88a948656ff73056aaf35198651f7a1
James Rhodes <james21198@gmail.com>
2024-04-27 11:34:06 +0100

    File Structure Added and NPM Packages Installed

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const user = require('./user');
+const post = require('./post');
+const comment = require('./comment');
+
+user.hasMany(post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+post.belongsTo(user, {
+    foreignKey: 'user_id'
+});
+
+post.hasMany(comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+comment.belongsTo(post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { user, post, comment };
