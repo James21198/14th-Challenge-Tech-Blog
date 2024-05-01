97bf40a08512e2907322c617b5f5d6bb42a05323
James Rhodes <james21198@gmail.com>
2024-05-01 21:31:50 +0100

    Comments Files Updated

--- public/js/post.js
+++ public/js/post.js
@@ -25,4 +25,31 @@ const editFormHandler = async(event) => {
     }
 }
 
+const addCommentHandler = async(event) => {
+    event.preventDefault();
+
+    const body_content = document.querySelector('input[name="body"]').value;
+    const id = window.location.toString().split('/') [
+        window.location.toString().split('/').length = 1
+    ];
+    const data = {
+        post_id: id,
+        body: body_content
+    }
+    const response = await fetch(`/api/comment/`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
 document.querySelector('edit-post-form').addEventListener('submit', editFormHandler);
+document.querySelector('comment-input').addEventListener('submit', addCommentHandler);;
