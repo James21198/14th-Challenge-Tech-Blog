87fed83661b31c5b1c29c5b408d40cbf3b083004
James Rhodes <james21198@gmail.com>
2024-05-09 20:25:40 +0100

    Connection Updated

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (!process.env.DB_URL) {
-  sequelize = new Sequelize("postgres://blog_db_rl2p_user:xSKu6D1wtVgXKw9tcNibKWuZ1QtZzTvc@dpg-cot4kqed3nmc73ds46f0-a/blog_db_rl2p");
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
